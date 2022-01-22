8ce41a66017ac318fd77a6692e7bd98d6a2894fd
devksh930 <ksh930@icloud.com>
2022-01-23 03:06:06 +0900

    init : project V2

---
+++ src/test/java/me/devksh930/bookstorage/BookstorageApplicationTests.java
@@ -0,0 +1,13 @@
+package me.devksh930.bookstorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstorageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
